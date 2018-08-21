15033ae4b6a9b201f1425a005276f14c1bb665ee
Andrei <3355844@gmail.com>
2018-08-21 20:22:22 +0300

    Human_class_with_description

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,18 @@ public class Main {
         String[] arr = new String[10];
         Menu menu = new Menu();
 
-        while (!menu.theEndProgram){
-            menu.mainMenu();
-            arr = menu.route(arr);
-        }
+        Human human = new Human();
+        Human human1 = new Human(10);
+        Human human2 = new Human(1, "name", "lastName", 30);
+
+        System.out.println(human.toString());
+        System.out.println(human1.toString());
+        System.out.println(human2.toString());
+
+
+//        while (!menu.theEndProgram){
+//            menu.mainMenu();
+//            arr = menu.route(arr);
+//        }
     }
 }
