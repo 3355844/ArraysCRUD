8535173b64f9b78bc8f2863968848d5b293d8ded
Andrei <3355844@gmail.com>
2018-08-23 19:26:05 +0300

    Add_class_Human_redactor_methods

--- src/HumanRedactor.java
+++ src/HumanRedactor.java
@@ -1,13 +1,44 @@
-public class HumanRedactor  {
+public class HumanRedactor {
 
-    public Human[] addHuman (Human[] arr, Human human){
-        for (int i = 0; i <arr.length; i++) {
-            if (arr[i]==null){
-                arr[i] = human;
-                return arr;
+    public Human[] addHuman(Human[] humans, Human human) {
+        for (int i = 0; i < humans.length; i++) {
+            if (humans[i] == null) {
+                humans[i] = human;
+                break;
             }
         }
-        return arr;
+        return humans;
     }
+
+    public void readHumans(Human[] humans) {
+
+        for (int i = 0; i < humans.length; i++) {
+            System.out.println(humans[i].toString());
+        }
+    }
+
+    public Human[] updateHuman(Human[] humans, Human human) {
+
+        for (int i = 0; i < humans.length; i++) {
+            if (humans[i].getId() == human.getId()) {
+                humans[i] = human;
+                break;
+            }
+        }
+        return humans;
+    }
+
+    public Human findById(Human[] humans, int id) {
+        Human human = null;
+        for (int i = 0; i < humans.length; i++) {
+            if (humans[i].getId() == human.getId()) {
+                human = humans[i];
+                break;
+            }
+        }
+        return human;
+    }
+
+
 
 }
