cdce9e28f2b9a79aaf3f3d1ba7d72895c1a4b1b9
Andrei <3355844@gmail.com>
2018-08-23 20:25:41 +0300

    Add_class_Human_router_update_method

--- src/HumanRouter.java
+++ src/HumanRouter.java
@@ -16,7 +16,24 @@ public class HumanRouter {
         return redactor.addHuman(humans, tmpHuman);
     }
 
-    public void readHumansRouting(Human [] humans){
+    public Human[] updateHumanRouting(Human[] humans) {
+        Human human;
+
+        System.out.println("Please enter Data for update Human:");
+        System.out.println("Enter ID for update:");
+        human = redactor.findById(humans, Integer.parseInt(menu.readConsoleValue()));
+        System.out.println("Enter new Name:");
+        human.setName(menu.readConsoleValue());
+        System.out.println("Enter new LastName:");
+        human.setLastName(menu.readConsoleValue());
+        System.out.println("Enter new Age");
+        human.setAge(Integer.parseInt(menu.readConsoleValue()));
+
+        return redactor.updateHuman(humans, human);
+    }
+
+
+    public void readHumansRouting(Human[] humans) {
         redactor.readHumans(humans);
     }
 
